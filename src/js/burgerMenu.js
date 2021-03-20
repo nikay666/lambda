d146ebf724ff5caf8ba1965201a63cb9583e8e2d
Nika Yakubova <lilit666an@gmail.com>
2021-03-20 18:00:00 +0300

    Fix: date input in safari, add a pattern for changing the date

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -2,19 +2,32 @@ export const burgerMenu = () => {
     const headerMenu = document.querySelector('.header__nav')
     const btnClose = headerMenu.querySelector('.btn-close')
     const toggleMenu = document.querySelector('.btn-hamburger')
+    const links = headerMenu.querySelectorAll('.nav__link')
     const activeCls = 'active'
     const isOpen = 'isOpen'
+
 
     toggleMenu.addEventListener('click', () => {
         headerMenu.classList.add(activeCls)
         toggleMenu.classList.add(isOpen)
     })
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            removeClass()
+        })
+    })
+    
     btnClose.addEventListener('click', () => {
-        headerMenu.classList.remove(activeCls)
-        toggleMenu.classList.remove(isOpen)
-
+        removeClass()
     })
 
     headerMenu.classList.remove(activeCls)
     toggleMenu.classList.remove(isOpen)
+
+
+    function removeClass(){
+        headerMenu.classList.remove(activeCls)
+        toggleMenu.classList.remove(isOpen)
+    }
 }
+
